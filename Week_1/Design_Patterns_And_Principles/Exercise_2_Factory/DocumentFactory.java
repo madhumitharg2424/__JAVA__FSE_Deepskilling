cf6b76252687ecba0142a0a05e36b4a273dc5f47
madhumitharg2424 <madhumitha0836@gmail.com>
2026-06-19 21:48:23 +0530

    factory

---
+++ Week_1/Design_Patterns_And_Principles/Exercise_2_Factory/DocumentFactory.java
@@ -0,0 +1,10 @@
+
+public abstract class DocumentFactory {
+
+    public abstract Document createDocument();
+
+    public void openDocument() {
+        Document doc = createDocument();
+        doc.open();
+    }
+}
